e065fa5bf8fa5a9665d9c2a6947c168df965f17e
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-11-15 10:37:35 +0200

    refactor(uoh-environment): Add module

---
+++ projects/uoh/ngx-tools/src/lib/modules/uoh-environment.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { UOH_ENVIRONMENT_FACTORY } from '../models/environment.model';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: [UOH_ENVIRONMENT_FACTORY],
+})
+export class NgxToolsModule {}
