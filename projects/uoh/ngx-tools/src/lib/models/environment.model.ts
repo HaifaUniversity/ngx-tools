b547b0777ca40d5ab3b7a99c498078314843133f
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-11-15 16:20:03 +0200

    refactor(uoh-environment): Move token & factory

--- projects/uoh/ngx-tools/src/lib/models/environment.model.ts
+++ projects/uoh/ngx-tools/src/lib/models/environment.model.ts
@@ -1,6 +1,3 @@
-import { DOCUMENT } from '@angular/common';
-import { FactoryProvider, InjectionToken } from '@angular/core';
-
 /**
  * Represents the different running environments.
  */
@@ -9,33 +6,3 @@ export enum UohEnvironment {
   QA = 'qa',
   Production = '',
 }
-
-/**
- * Retrieves the environment the app is currently running on.
- * @param document The HTML document.
- */
-export function getEnvironment(document: Document): UohEnvironment {
-  if (document.location.hostname.includes(UohEnvironment.Development)) {
-    return UohEnvironment.Development;
-  } else if (document.location.hostname.includes(UohEnvironment.QA)) {
-    return UohEnvironment.QA;
-  } else {
-    return UohEnvironment.Production;
-  }
-}
-
-/**
- * A token to inject the running environment.
- */
-export const UOH_ENVIRONMENT = new InjectionToken<UohEnvironment>(
-  'The environment this app is running on.'
-);
-
-/**
- * A factory that injects the running environment.
- */
-export const UOH_ENVIRONMENT_FACTORY: FactoryProvider = {
-  provide: UOH_ENVIRONMENT,
-  useFactory: getEnvironment,
-  deps: [DOCUMENT],
-};
--- projects/uoh/ngx-tools/src/lib/modules/uoh-environment.module.ts
+++ projects/uoh/ngx-tools/src/lib/modules/uoh-environment.module.ts
@@ -1,9 +1,40 @@
-import { NgModule } from '@angular/core';
-import { UOH_ENVIRONMENT_FACTORY } from '../models/environment.model';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { FactoryProvider, InjectionToken, NgModule } from '@angular/core';
+import { UohEnvironment } from '../models/environment.model';
+
+/**
+ * Retrieves the environment the app is currently running on.
+ * @param document The HTML document.
+ */
+export function getEnvironment(document: Document): UohEnvironment {
+  if (document.location.hostname.includes(UohEnvironment.Development)) {
+    return UohEnvironment.Development;
+  } else if (document.location.hostname.includes(UohEnvironment.QA)) {
+    return UohEnvironment.QA;
+  } else {
+    return UohEnvironment.Production;
+  }
+}
+
+/**
+ * A token to inject the running environment.
+ */
+export const UOH_ENVIRONMENT = new InjectionToken<UohEnvironment>(
+  'The environment this app is running on.'
+);
+
+/**
+ * A factory that injects the running environment.
+ */
+export const UOH_ENVIRONMENT_FACTORY: FactoryProvider = {
+  provide: UOH_ENVIRONMENT,
+  useFactory: getEnvironment,
+  deps: [DOCUMENT],
+};
 
 @NgModule({
   declarations: [],
-  imports: [],
+  imports: [CommonModule],
   exports: [],
   providers: [UOH_ENVIRONMENT_FACTORY],
 })
